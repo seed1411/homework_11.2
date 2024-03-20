7cf251af9d23f65479e0ec91851178bc715f1e09
Anton Gromov <seed14112@yandex.ru>
2024-03-20 10:19:34 +0300

    add def word_upper and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def word_upper(value):
+    """
+    Делает все буквы заглавные
+    """
     return value.upper()
+
+
+def word_title(value):
+    """
+    Делает первую букву заглавной
+    """
+    return value.title()
